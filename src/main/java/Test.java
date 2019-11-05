91278f86fdb15468a9de57aa8fdafbf04424c0ee
zhengjianxin <1972750829@qq.com>
2019-11-05 21:38:37 +0800

    git第二次测试

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello git,I'm idea");
+        System.out.println("Hello git,I'm idea and come here");
     }
 }
